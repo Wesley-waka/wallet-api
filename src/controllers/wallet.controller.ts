7d6d8c4d6645844c8ffdc298334eeba28a5a5937
Wesley Waka <wesleywaka2@gmail.com>
2026-03-14 17:16:16 +0300

    chore:refactor and modularize common operations into a single directory

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,7 +1,7 @@
-
-
 import { Request, Response } from "express";
 import prisma from "../lib/prisma";
+import { createTransaction, TransactionType } from "../services/transaction.service";
+import { creditWallet, debitWallet, getWalletValidate } from "../services/wallet.service";
 
 export async function createWallet(req: Request, res: Response) {
     const { username } = req.body;
@@ -35,25 +35,9 @@ export async function depositWallet(req: Request, res: Response) {
     
     
     try{
-        const wallet = await prisma.wallet.update({
-            where: {
-                userName: username
-            },
-            data: {
-                balance: {
-                    increment: amount
-                }
-            }
-        });
+        const wallet = await depositWallet(username, amount);
 
-        await prisma.transaction.create({
-            data: {
-                from: null,
-                to: username,
-                amount: amount,
-                type: "deposit"
-            }
-        });
+        await createTransaction(null, username, amount, TransactionType.DEPOSIT);
 
         res.status(200).json(wallet);
     } catch (error) {
@@ -94,95 +78,28 @@ export async function transferFunds(req: Request, res: Response) {
         return res.status(400).json({ error: "User funded and amount are required" });
     }
 
-    const userFundedWallet = await prisma.wallet.findUnique({
-        where: {
-            userName: userFunded
-        }
-    });
+    const userFundedWallet = await getWalletValidate(userFunded);
 
     if(!userFundedWallet){
         return res.status(404).json({ error: "User funded not found" });
     }
 
-    
-
     try{
 
-        await prisma.wallet.update({
-            where: {
-                userName: username
-            },
-            data: {
-                balance: {
-                    decrement: amount
-                }
-            }
-        });
-        
-        await prisma.wallet.update({
-            where: {
-                userName: userFunded
-            },
-            data: {
-                balance: {
-                    increment: amount
-                }
-            }
-        });
 
-        await prisma.transaction.create({
-            data: {
-                sender_wallet_id: username,
-                receiver_wallet_id: userFunded,
-                amount: amount,
-                type: "SENT"
-            }
-        });
+        await debitWallet(username, amount);
+        await creditWallet(userFunded, amount);
 
-        // await prisma.transaction.create({
-        //     data: {
-        //         from: userFunded,
-        //         to: username,
-        //         amount: amount,
-        //         type: "RECEIVED"
-        //     }
-        // });
-        
+
+        await createTransaction(username, userFunded, amount, TransactionType.SENT);
+        await createTransaction(userFunded, username, amount, TransactionType.RECEIVED);
+   
         res.status(200).json({ message: "Transfer successful" });
     } catch (error) {
         res.status(500).json({ error: "Internal server error" });
-    }
-    
-    
+    }  
 }
 
-export async function getTransactions(req: Request, res: Response) {
-    const { username,transactionType } = req.body;
-
-    if(!username){
-        return res.status(400).json({ error: "Username is required" });
-    }
-
-    if(transactionType && (transactionType !== "SENT" && transactionType !== "RECEIVED" && transactionType !== "DEPOSIT" && transactionType !== "WITHDRAW")){
-        return res.status(400).json({ error: "Transaction type must be SENT, RECEIVED, DEPOSIT or WITHDRAW" });
-    }
-
-    try{
-       const transactions = await prisma.transaction.findMany({
-            where: {
-                type: transactionType || undefined,
-                OR: [
-                { sender_wallet: { userName: username } },
-                { receiver_wallet: { userName: username } }
-                ]
-            }
-            });
-
-        res.status(200).json(transactions);
-    } catch (error) {
-        res.status(500).json({ error: "Internal server error" });
-    }
-}
 
 export async function withdrawWallet(req: Request, res: Response) {
     const { username, amount } = req.body;
@@ -193,35 +110,14 @@ export async function withdrawWallet(req: Request, res: Response) {
 
     if (username === "") {
         return res.status(400).json({ error: "Username cannot be empty" });
-    }
-
-    
-    
+    }  
     try{
-        const wallet = await prisma.wallet.update({
-            where: {
-                userName: username
-            },
-            data: {
-                balance: {
-                    decrement: amount
-                }
-            }
-        });
+        const wallet = await withdrawWallet(username, amount);
 
-        await prisma.transaction.create({
-            data: {
-                sender_wallet_id: username,
-                receiver_wallet_id: null,
-                amount: amount,
-                type: "WITHDRAW"
-            }
-        });
+        await createTransaction(username, null, amount, TransactionType.WITHDRAWAL);
 
         res.status(200).json(wallet);
     } catch (error) {
         res.status(500).json({ error: "Internal server error" });
     }
 }
-
-
