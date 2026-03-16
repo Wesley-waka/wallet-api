3712ebc3d2d5b73943163c9d8c4f44ac2e4ca185
Wesley Waka <wesleywaka2@gmail.com>
2026-03-16 16:49:00 +0300

    feat:add route for soft delete wallet

--- src/routes/wallet.route.ts
+++ src/routes/wallet.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createWallet, depositWallet, withdrawWallet, transferFunds, getWallet, getWalletBalance } from "../controllers/wallet.controller";
+import { createWallet, depositWallet, withdrawWallet, transferFunds, getWallet, getWalletBalance, deleteWallet } from "../controllers/wallet.controller";
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router.post("/withdraw", withdrawWallet);
 router.post("/transfer", transferFunds);
 router.get('/:username', getWallet);
 router.get('/:username/balance', getWalletBalance);
+router.delete("/", deleteWallet);
 
 export default router;
 
