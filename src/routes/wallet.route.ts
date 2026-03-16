d78c6147d2c0eeaf1967835b07318d9ac24c05ad
Wesley Waka <wesleywaka2@gmail.com>
2026-03-16 16:34:41 +0300

    feat:add wallet balance

--- src/routes/wallet.route.ts
+++ src/routes/wallet.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createWallet, depositWallet, withdrawWallet, transferFunds, getWallet } from "../controllers/wallet.controller";
+import { createWallet, depositWallet, withdrawWallet, transferFunds, getWallet, getWalletBalance } from "../controllers/wallet.controller";
 
 const router = Router();
 
@@ -8,6 +8,7 @@ router.post("/deposit", depositWallet);
 router.post("/withdraw", withdrawWallet);
 router.post("/transfer", transferFunds);
 router.get('/:username', getWallet);
+router.get('/:username/balance', getWalletBalance);
 
 export default router;
 
